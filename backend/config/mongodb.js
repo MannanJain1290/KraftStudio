eff234d4f5968e0f1ad6c9a22e0e57697c749a20
Mannan Jain <jainmannan8123@gmail.com>
2026-08-22 22:35:27 +0530

    backend

---
+++ backend/config/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"; 
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+
+}
+
+export default connectDB;
